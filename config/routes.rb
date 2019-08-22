ae56c0ca3d4ae230bc022c931b4018be065b1644
shaqnawe <shaqnawe@gmail.com>
2019-08-21 21:07:02 -0500

    Contact File Updated

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 get 'welcome/index'
 get 'contacts/new'
 get 'articles/articles'
+get '/contacts' => 'contacts#create'
 
 resources :contacts, only: [:new, :create]
 
