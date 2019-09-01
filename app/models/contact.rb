f7ab57e64c522747ec89b8780cce5c3a2892ee5f
shaqnawe <shaqnawe@gmail.com>
2019-09-01 18:53:41 -0500

    removed file attribute from contact.

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,7 +1,6 @@
 class Contact < MailForm::Base
 	attribute :name,		:validate => true
 	attribute :email,		:validate => /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
-	attribute :file,      	:attachment => true
 	attribute :message,		:validate => true
 	attribute :nickname,	:captcha => true
 
