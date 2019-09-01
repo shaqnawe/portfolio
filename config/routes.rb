483e27049371c5e94fd4c9488101f397e70acacb
shaqnawe <shaqnawe@gmail.com>
2019-09-01 18:29:49 -0500

    Added Sendgrid production configs.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+resources :posts
+resources :projects
+resources :contacts, only: [:new, :create]
+
 get 'welcome/index'
 get 'contacts/new'
 get 'articles/articles'
